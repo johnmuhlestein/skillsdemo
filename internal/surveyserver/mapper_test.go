ffa237894262ca9d2747610e57bf1835206fa2ce
John Muhlestein <john.muhlestein@deepsee.ai>
2024-04-08 17:28:36 -0600

    this is it

--- internal/surveyserver/mapper_test.go
+++ internal/surveyserver/mapper_test.go
@@ -2,6 +2,7 @@ package surveyserver
 
 import (
 	"skillsdemo/ent"
+	"skillsdemo/ent/schema"
 	"testing"
 
 	"github.com/google/uuid"
@@ -13,4 +14,20 @@ func TestMapDbFeedback(t *testing.T) {
 	if feedbackResp.Status != "created" {
 		t.Errorf("got %s, wanted %s", feedbackResp.Status, "created")
 	} 
+}
+
+func TestMapDbMeasureEnumeration(t *testing.T) {
+	dbEnums := []schema.MeasureEnum{{Label: "FirstThing", Value:1},{Label:"SecondThing",Value:2}}
+	pbEnums := mapDbMeasureEnumeration(dbEnums)
+	if len(pbEnums) != len(dbEnums) {
+		t.Errorf("enum mapper did not return the array of the correct lenght expected %d got %d", len(dbEnums), len(pbEnums))
+	}
+}
+
+func TestMapDbMeasureFlags(t *testing.T) {
+	dbFlags := []string{"yellow","green","blue","red","purple"}
+	pbFlags := mapDbMeasureFlags(dbFlags)
+	if len(dbFlags) != len(pbFlags) {
+		t.Errorf("flag mapper did not return the array with tht correct lenght. expected %d got %d", len(dbFlags), len(pbFlags))
+	}
 }
